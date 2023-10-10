31c137d2689794246fac65ff5ca901238ef1c138
Rachel Walmsley <rachwalm75@gmail.com>
2023-10-10 14:51:49 +0100

    Fixed cluster markers on map in interests

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,26 +1,28 @@
-function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
+async function initMap() {
+    // Request needed libraries.
+    const {
+        Map,
+        InfoWindow
+    } = await google.maps.importLibrary("maps");
+    const {
+        AdvancedMarkerElement,
+        PinElement
+    } = await google.maps.importLibrary(
+        "marker",
+    );
+
+    const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
             lat: 46.619261,
             lng: -33.134766
-        }
+        },
+        mapId: "DEMO_MAP_ID",
     });
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var location = [{
-            lat: 40.785091,
-            lng: -73.968285
-        },
-        {
-            lat: 41.084045,
-            lng: -73.874245
-        },
-        {
-            lat: 40.754932,
-            lng: -73.984016
-        }
-    ];
 
+    // Create an array of alphabetical characters used to label the markers.
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // Add some markers to the map.      
     var markers = locations.map(function (location, i) {
         return new google.maps.Marker({
             position: location,
@@ -28,8 +30,25 @@ function initMap() {
         });
     });
 
-    new MarkerClusterer({
-        markers,
-        map
+    // Add a marker clusterer to manage the markers.
+    const markerCluster = new markerClusterer.MarkerClusterer({
+        map,
+        markers
     });
 }
+
+const locations = [{
+        lat: 40.785091,
+        lng: -73.968285
+    },
+    {
+        lat: 41.084045,
+        lng: -73.874245
+    },
+    {
+        lat: 40.754932,
+        lng: -73.984016
+    }
+];
+
+initMap();
